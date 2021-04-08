45a1fa07564f877960148c254568281004b4c5a3
Michael Poveda <mapovedaa@gmail.com>
2021-04-08 11:54:10 -0500

    Red neuronal funcional

--- sk_learn.py
+++ sk_learn.py
@@ -81,43 +81,46 @@ def multiLayerPerceptron(X_test, y_test, X_train, y_train, lr, alpha, hls):
 
     return accuracy
 
+def main():
+    pathCSV = str(input("please, put path file csv (default = data.csv): ") or "data.csv")
+    csv = readFile(pathCSV)
+    dataset = configCSV(csv)
 
-pathCSV = str(input("please, put path file csv (default = data.csv): ") or "data.csv")
-csv = readFile(pathCSV)
-dataset = configCSV(csv)
+    X, y    = extractDataCSV(dataset, 'infected')
+    X       = removeAtipicalData(X, .8 ,0)
 
-X, y    = extractDataCSV(dataset, 'infected')
-X       = removeAtipicalData(X, .8 ,0)
+    #analisisExploratorio(dataset)
+    #distVarNumericas(dataset, ['float64', 'int'])
 
-#analisisExploratorio(dataset)
-#distVarNumericas(dataset, ['float64', 'int'])
+    #corr_matrix = dataset.select_dtypes(include=['float64', 'int']).corr(method='pearson')
+    #heatmapCorrMatrix(corr_matrix)
 
-#corr_matrix = dataset.select_dtypes(include=['float64', 'int']).corr(method='pearson')
-#heatmapCorrMatrix(corr_matrix)
+    # Transformaciones para las variables numéricas
+    numeric_cols = X.select_dtypes(include=['float64', 'int']).columns.to_list()
+    numeric_transformer = Pipeline(
+                            steps=[ ('imputer', SimpleImputer(strategy='median')),
+                                    ('scaler',  StandardScaler()                )])
 
-# Transformaciones para las variables numéricas
-numeric_cols = X.select_dtypes(include=['float64', 'int']).columns.to_list()
-numeric_transformer = Pipeline(
-                        steps=[ ('imputer', SimpleImputer(strategy='median')),
-                                ('scaler',  StandardScaler()                )])
+    preprocessor = ColumnTransformer(
+                        transformers=[
+                            ('numeric', numeric_transformer, numeric_cols),
+                        ],
+                        remainder='passthrough'
+                    )
 
-preprocessor = ColumnTransformer(
-                    transformers=[
-                        ('numeric', numeric_transformer, numeric_cols),
-                    ],
-                    remainder='passthrough'
-                )
+    X_prep = preprocessor.fit_transform(X)
+    labels = np.concatenate([numeric_cols])
+    X_prep = preprocessor.transform(X)
+    X_prep = pd.DataFrame(X_prep, columns=labels)
+    # División de los datos en train y test
+    # ==============================================================================
+    X_train, X_test, y_train, y_test = train_test_split(X_prep                ,
+                                                        y.values.reshape(-1,1),
+                                                        train_size   = 0.7    ,
+                                                        random_state = 1234   ,
+                                                        shuffle      = True   )
 
-X_prep = preprocessor.fit_transform(X)
-labels = np.concatenate([numeric_cols])
-X_prep = preprocessor.transform(X)
-X_prep = pd.DataFrame(X_prep, columns=labels)
-# División de los datos en train y test
-# ==============================================================================
-X_train, X_test, y_train, y_test = train_test_split(X_prep                ,
-                                                    y.values.reshape(-1,1),
-                                                    train_size   = 0.7    ,
-                                                    random_state = 1234   ,
-                                                    shuffle      = True   )
+    _ = multiLayerPerceptron(X_test, y_test, X_train, y_train, 1e-2, 1e-3, (45,12,6,3,))
 
-_ = multiLayerPerceptron(X_test, y_test, X_train, y_train, 1e-2, 1e-3, (45,12,6,3,))
+if __name__ == "__main__":
+    main()
